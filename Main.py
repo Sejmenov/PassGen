1897f0662dec3d26efbafcd3275da94436fa42d4
Sejmenov <34370980+Sejmenov@users.noreply.github.com>
2017-12-10 11:24:18 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -3,37 +3,19 @@ Created on 9 Dec 2017
 
 @author: sejmenov
 '''
+
 import random
 
-def random_symbol(let):
-    abc = "qwertyuiopasdfghjklzxcvbnm"
-    ABC = "QWERTYUIOPASDFGHJKLZXCVBNM"
-    num = "0123456789"
-    NUM = "!@#$*"
-    a = random.randint(0,25)
-    b = random.randint(0,25)
-    c = random.randint(0,9)
-    d = random.randint(0,4)
-    if let == "a":
-        return abc[a]
-    elif let == "b":
-        return ABC[b]
-    elif let == "c":
-        return num[c]
-    else:
-        return NUM[d]
-
 def passgen():
-    password = ["a","b","c","d","e","f","g","h","j","k"]
-    for i in range(10):
-        password[i]= random_symbol(random.choice("abcd"))
+    password = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    for i in range(20):
+        password[i] = chr(random.randint(33, 127))
         i += 1
     return ''.join(password)
 
-n = int(input("How many 10 letter passwords do you want to create?:"))
+n = int(input("How many 20 symbols long passwords do you want to create?:"))
 file = open("Password.txt", "w+")
 for i in range(n):
     print(i+1," ",passgen(),file=file)
     i +=1
 file.close()
-
